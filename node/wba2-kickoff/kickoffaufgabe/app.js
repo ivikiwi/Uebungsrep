9d42e389e8341e34e506ef8829ce63efc95e63da
P.T.D.Ivanka Vo <ivanka.vo@googlemail.com>
2015-04-20 23:12:35 +0200

    added object with name infos to parse json-data and data output

--- node/wba2-kickoff/kickoffaufgabe/app.js
+++ node/wba2-kickoff/kickoffaufgabe/app.js
@@ -1,3 +1,15 @@
 var fs = require('fs');
 
-fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) { ... });
+fs.readFile(__dirname+"/wolkenkratzer.json", function(err, data) {
+            if (err) throw err;
+
+              var infos = JSON.parse(data.toString());
+
+              for(var i in infos.wolkenkratzer) {
+              console.log(infos.wolkenkratzer[i].name);
+              console.log(infos.wolkenkratzer[i].stadt);
+              console.log(infos.wolkenkratzer[i].hoehe);
+            }
+
+
+ });
