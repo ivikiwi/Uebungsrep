e2cd93295f6a5fda3b3509178a35c32656460a6e
Ivanka <ivankavo1@Ivankas-MBP.fritz.box>
2015-05-13 00:53:01 +0200

    added series-json data and get with simple json response

--- Express_Serienliste/app.js
+++ Express_Serienliste/app.js
@@ -4,3 +4,19 @@ var jsonParser = bodyParse.json();
 
 var app = express();
 
+var series = [
+	{
+		"name": "Once Upon a Time",
+		"seasons": "4"
+	},
+	{
+		"name": "Pretty little liar",
+		"seasons": "3"
+	}
+]
+
+app.get('/', function(req, res) {
+	res.status(200).json(series);
+});
+
+app.listen(8888);
