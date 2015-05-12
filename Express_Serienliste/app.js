c26c30b6f76f9807bddd87c7e9ced9489521ce23
Ivanka <ivankavo1@Ivankas-MBP.fritz.box>
2015-05-13 01:05:59 +0200

    changed get response to negotiation type html and json

--- Express_Serienliste/app.js
+++ Express_Serienliste/app.js
@@ -6,17 +6,27 @@ var app = express();
 
 var series = [
 	{
-		"name": "Once Upon a Time",
-		"seasons": "4"
+		name: "Once Upon a Time",
+		seasons: "4"
 	},
 	{
-		"name": "Pretty little liar",
-		"seasons": "3"
+		name: "Pretty little liar",
+		seasons: "3"
 	}
 ]
 
 app.get('/', function(req, res) {
-	res.status(200).json(series);
+	var acceptedTypes = req.accepts(['html', 'json']);
+	switch(acceptedTypes) {
+		case 'html':
+			res.type('html').send('<h1>'+JSON.stringify(series)+'</h1>');
+			break;
+		case 'json':
+			res.json(series);
+			break;
+		default:
+			res.status(406).end();
+	}
 });
 
 app.listen(8888);
